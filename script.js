e375d2a7858ec513581d7c4204b7f04cee40d647
Reuben Germain <reuben.germain@soprasteria.com>
2024-06-23 22:56:29 +0100

    created an input alert, so if a user click inside the empty box it will alert 'please enter a city'

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getWeather() {
     const apiKey = `7b2321a733bb18916e05fdcee6f22156`
     const city = document.getElementById('city').value
+
+    if (!city) {
+        alert('Please enter a city')
+        return;
+    }
 }
